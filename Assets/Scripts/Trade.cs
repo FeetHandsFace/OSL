5f7ce75daacb2eba056f4fbf78a9f2f966820325
FeetHandsFace <isa@icewatergames.com>
2016-06-02 14:48:11 -0700

    forgot to save trade script

--- Assets/Scripts/Trade.cs
+++ Assets/Scripts/Trade.cs
@@ -4,20 +4,21 @@ using System.Collections;
 [RequireComponent(typeof(SphereCollider))]
 public class Trade : MonoBehaviour {
 
-	public int identityNumber;
 	Merchant merchant;
 	SphereCollider sphereCollider;
 	bool isHighlighted = false;
-	
+
 	/*Relationship with a trading source, things to save:
 		How many times they have been stolen from
 		How many times they have been short changed
 		*/
 
 	// Use this for initialization
-	void Start () {
+	void OnLevelWasLoaded(int level) {
 		//pulls a random 
-		merchant = Persistant.persist.merchants[identityNumber];
+		//IMPORTANT!!-----------------all the merchants must be blocked together in the scene manager
+		merchant = Persistant.persist.merchants[level - 4];
+		merchant.changeOutSecrets();
 		sphereCollider = GetComponent<SphereCollider>();
 		sphereCollider.isTrigger = true;
 	}
